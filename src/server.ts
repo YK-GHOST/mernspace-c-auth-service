70c6f04beefc04a0548c52638211bbb7c96d1e5a
YK-GHOST <yogeshkantiwal20@gmail.com>
2024-12-12 07:39:29 +0530

    test lint-staged 4

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 function welcome(name: string) {
     console.log(`Welcome ${name}`);
+
+    const player = {
+        name: "Ghost",
+    };
+
+    const fname = player.name;
+
+    return fname;
 }
 
 welcome("ghost");
