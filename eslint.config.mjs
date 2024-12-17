c5130b7fbb8523b7ce26b672eb41e2135488d74e
YK-GHOST <yogeshkantiwal20@gmail.com>
2024-12-17 16:49:21 +0530

    add test setup with jest and supertest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,12 @@ export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
     {
-        ignores: ["dist", "node_modules", "eslint.config.mjs"],
+        ignores: [
+            "dist",
+            "node_modules",
+            "eslint.config.mjs",
+            "jest.config.js",
+        ],
     },
     {
         languageOptions: {
@@ -20,6 +25,7 @@ export default tseslint.config(
         rules: {
             // "no-console": "warn",
             "dot-notation": "error",
+            "@typescript-eslint/no-misused-promises": "off",
         },
     },
 );
