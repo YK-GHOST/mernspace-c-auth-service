a07d9ce4af404db1ea522700d32cee6d88ab4ddd
YK-GHOST <yogeshkantiwal20@gmail.com>
2024-12-24 13:42:08 +0530

    add test for presist user & add configs for prettier

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,8 +1,27 @@
 import request from "supertest";
 import app from "../../src/app";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../src/config/data-source";
+import { truncateTables } from "../utils";
+import { User } from "../../src/entity/User";
 
 describe("POST /auth/register", () => {
     describe("Given all fields", () => {
+        let connection: DataSource;
+
+        beforeAll(async () => {
+            connection = await AppDataSource.initialize();
+        });
+
+        beforeEach(async () => {
+            //Database Truncate
+            await truncateTables(connection);
+        });
+
+        afterAll(async () => {
+            await connection.destroy();
+        });
+
         it("should return 201 status code", async () => {
             /**
              * Format to write test.
@@ -44,6 +63,27 @@ describe("POST /auth/register", () => {
             expect(
                 (response.headers as Record<string, string>)["content-type"],
             ).toEqual(expect.stringContaining("json"));
+        });
+
+        it("should presist the user in database", async () => {
+            //Arrange
+            const userData = {
+                firstName: "Yogesh",
+                lastName: "Kantiwal",
+                email: "yogesh@gmail.com",
+                password: "secret",
+            };
+
+            //Act
+            await request(app).post("/auth/register").send(userData);
+
+            //Assert
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            expect(users).toHaveLength(1);
+            expect(users[0].firstName).toBe(userData.firstName);
+            expect(users[0].lastName).toBe(userData.lastName);
+            expect(users[0].email).toBe(userData.email);
         });
     });
     describe("Fields are missing", () => {});
