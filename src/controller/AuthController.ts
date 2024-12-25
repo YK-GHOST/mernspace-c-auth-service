b201cfce81fa8aaea86426f6d190a6fc1bb22241
YK-GHOST <yogeshkantiwal20@gmail.com>
2024-12-25 14:24:22 +0530

    add errorHandling and logging

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,19 +1,39 @@
-import { Response } from "express";
+import { NextFunction, Response } from "express";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
+import { Logger } from "winston";
 
 export class AuthController {
     //Dependency Injection -> recieving dependencies from the constructor
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private logger: Logger,
+    ) {}
 
-    async register(req: RegisterUserRequest, res: Response) {
-        const { firstName, lastName, email, password } = req.body;
-        const user = await this.userService.create({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
-        res.status(201).json({ id: user.id });
+    async register(
+        req: RegisterUserRequest,
+        res: Response,
+        next: NextFunction,
+    ) {
+        try {
+            const { firstName, lastName, email, password } = req.body;
+            this.logger.debug("New request to register a user.", {
+                firstName,
+                lastName,
+                email,
+                password: "******",
+            });
+            const user = await this.userService.create({
+                firstName,
+                lastName,
+                email,
+                password,
+            });
+            this.logger.info("User has been registered", { id: user.id });
+            res.status(201).json({ id: user.id });
+        } catch (err) {
+            next(err);
+            return;
+        }
     }
 }
