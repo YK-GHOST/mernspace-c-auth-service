7ec5b88cf0336b699ec824f42d2a70868ae21bcb
YK-GHOST <yogeshkantiwal20@gmail.com>
2024-12-29 11:39:36 +0530

    add test for email sanitaization

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -1,3 +1,9 @@
-import { body } from "express-validator";
-
-export default [body("email").notEmpty().withMessage("Email is required!")];
+import { checkSchema } from "express-validator";
+export default checkSchema({
+    email: {
+        errorMessage: "Email is required!",
+        notEmpty: true,
+        trim: true,
+    },
+});
+// export default [body("email").notEmpty().withMessage("Email is required!")];
