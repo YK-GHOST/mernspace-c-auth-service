fa1a313461e0d1c3c330e3ce7f1b44c95c939ed1
YK-GHOST <yogeshkantiwal20@gmail.com>
2025-01-07 21:20:40 +0530

    refactor RefreshToken

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -4,8 +4,6 @@ import { JwtPayload } from "jsonwebtoken";
 import { UserService } from "../services/UserService";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
-import { AppDataSource } from "../config/data-source";
-import { RefreshToken } from "../entity/RefreshToken";
 import { TokenService } from "../services/TokenService";
 
 export class AuthController {
@@ -52,20 +50,8 @@ export class AuthController {
             const accessToken = this.tokenService.generateAccessToken(payload);
 
             //Presist the refreshToken
-            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
-            const refreshTokenRepository =
-                AppDataSource.getRepository(RefreshToken);
-            const newRefreshToken = await refreshTokenRepository.save({
-                user: user,
-                expiresAt: new Date(Date.now() + MS_IN_YEAR),
-            });
-
-            // const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
-            //     algorithm: "HS256",
-            //     expiresIn: "1y",
-            //     issuer: "auth-service",
-            //     jwtid: String(newRefreshToken.id),
-            // });
+            const newRefreshToken =
+                await this.tokenService.presistRefreshToken(user);
 
             const refreshToken = this.tokenService.generateRefreshToken({
                 ...payload,
