60cc4d36ad517b06e5345000b21be7dcb712598f
YK-GHOST <yogeshkantiwal20@gmail.com>
2025-03-16 17:59:42 +0530

    change private key read strategy

--- src/config/index.ts
+++ src/config/index.ts
@@ -17,6 +17,7 @@ const {
     DB_NAME,
     REFRESH_TOKEN_SECRET,
     JWKS_URI,
+    PRIVATE_KEY,
 } = process.env;
 
 export const Config = {
@@ -29,4 +30,5 @@ export const Config = {
     DB_NAME,
     REFRESH_TOKEN_SECRET,
     JWKS_URI,
+    PRIVATE_KEY,
 };
